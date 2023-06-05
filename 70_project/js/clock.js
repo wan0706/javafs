927cdb8bb5cfdc92d3ae4a31d7b1fbbd3712bf65
wan0706 <wan0706@hanmail.net>
2023-06-05 18:16:43 +0900

    0605

---
+++ 70_project/js/clock.js
@@ -0,0 +1,108 @@
+
+/*
+    문서의 DOM 내용을 전부 읽은 후 함수를 실행
+
+    자바스크립트
+    document.addEventListener('DOMContentLoaded', function(){});
+
+    제이쿼리
+    $(funciton(){});
+
+*/
+//문서의 DOM 내용을 전부 읽은 후 함수를 실행
+document.addEventListener('DOMContentLoaded', function(){
+
+//변수
+const yArea = document.querySelector('.yy'),
+oArea = document.querySelector('.mo'),
+dArea = document.querySelector('.dd'),
+wArea = document.querySelector('.we'),
+hArea = document.querySelector('.hh'),
+mArea = document.querySelector('.mm'),
+sArea = document.querySelector('.ss');
+
+setInterval(timer, 1000);
+
+function timer() {
+
+   //날짜와 시간 d 객체 생성
+   let d = new Date();
+
+  
+   // 년(yyyy)
+  yArea.innerHTML = d.getFullYear() + '년';
+   // 월(mm) 0(1월) ~ 11(12월)
+  oArea.innerHTML = d.getMonth() + 1 + '월';
+   // 일(dd)
+  dArea.innerHTML = d.getDate() + '일';
+   // 요일(week): 0(일) ~ 6(토)
+   // 요일을 일요일 ~ 토요일까지 문자열로 표시하기
+   // if를 사용하여 요일을 일요일 ~ 토요일까지 문자열로 표시하기
+
+
+   let week = d.getDay();
+
+   if (week === 0) {
+       week = '일요일';
+   } else if (week === 1) {
+       week = '월요일';
+   } else if (week === 2) {
+       week = '화요일';
+   } else if (week === 3) {
+       week = '수요일';
+   } else if (week === 4) {
+       week = '목요일';
+   } else if (week === 5) {
+       week = '금요일';
+   } else {
+       week = '토요일';
+   }
+
+  wArea.innerHTML = week;
+
+
+   // 시(hh)
+   // 오전과 오후를 구분하여 12시간제로 표시하기
+   // 예) 오후 5시
+
+   let hh = d.getHours();
+
+   if (hh >= 13) {
+       // 13 ~ 23
+       hh = 'PM ' + (hh - 12) + '시';
+       // 12
+   } else if (hh >= 12){
+       hh = 'PM ' + hh + '시';
+       // 10 ~ 11
+   } else if (hh >= 10){
+       hh = 'AM ' + hh + '시';
+       //0 ~ 9
+   } else {
+       hh = 'AM 0' + hh + '시';
+   }
+
+   hArea.innerHTML = hh;
+
+   // 분(mm)
+   // 00 ~ 09, 10 ~ 60
+   let mm = d.getMinutes();
+   let ss = d.getSeconds();
+
+
+   if (mm < 10) {
+       mm = '0' + mm;
+   }
+
+   if (ss < 10) {
+       ss = '0' + ss;
+   }
+
+
+   mArea.innerHTML = mm + '분';
+   sArea.innerHTML = ss + '초';
+
+
+
+}  
+
+});
