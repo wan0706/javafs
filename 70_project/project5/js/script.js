3f8dd8b5abbf300e3132bcebd29b66d01a7ade29
wan0706 <wan0706@hanmail.net>
2023-06-14 17:28:52 +0900

    0614

--- 70_project/project5/js/script.js
+++ 70_project/project5/js/script.js
@@ -92,26 +92,11 @@ $(function () {
         return false;
     });
 
-    // 풀페이지 레이아웃
-    $('.section').mousewheel(function (e, delta) {
-        let prev;
-        if (delta > 0) {
-            prev = $(this).prev().offset().top;
-            console.log(prev);
-            $('html').stop().animate({ scrollTop: prev }, 400, 'easeOutExpo');
-        } else if (delta < 0) {
-            let next = $(this).next().offset().top;
-            console.log(next);
-            $('html').stop().animate({ scrollTop: next }, 400, 'easeOutExpo');
-        }
-    });
-
-    //섹션2
+    // 섹션2 - ???
     const sec2 = $('#section2'),
         btn = sec2.find('.btn'),
         txt1 = sec2.find('.txt1'),
         txt2 = sec2.find('.txt2');
-
 
     $(window).scroll(function () {
         let st = $(this).scrollTop();
@@ -119,13 +104,71 @@ $(function () {
         console.log(st);
 
         if (st >= stVal) {
-            btn.css({ opacity: 1 });
-            txt1.css({ left: 360 +'px'});
-            txt2.css({ left: 360 +'px'});
+            btn.css({opacity: 1});
+            txt1.css({left: 360 + 'px'});
+            txt2.css({left: 360 + 'px'});
         } else {
-            btn.css({ opacity: 0 });
-            txt1.css({ left: -800 +'px'});
-            txt2.css({ left: -400 +'px'});
+            btn.css({opacity: 0});
+            txt1.css({left: -800 + 'px'});
+            txt2.css({left: -400 + 'px'});
+        }
+    });
+
+    // 섹션 3
+    const tabBtn = $('#section3 .thumb li'),
+    bigImg = $('#section3 .big li'),
+    txt = $('#section3 .txt li');
+
+tabBtn.click(function(){
+  let idx = $(this).index();
+  tabBtn.removeClass('active');
+  bigImg.removeClass('active');
+  txt.removeClass('active');
+  $(this).addClass('active');
+  bigImg.eq(idx).addClass('active');
+  txt.eq(idx).addClass('active');
+});
+
+
+    // 풀페이지 레이아웃
+    $('html').stop().animate({ scrollTop: 0 });
+
+    $('#indicator a').click(indicator);
+
+    function indicator() {
+        let idx = $(this).parent().index();
+        console.log(idx);
+        let posY = $('.section').eq(idx).offset().top;
+        $('html,body').stop().animate({ scrollTop: posY });
+        tooltip(idx);
+    }
+
+    function tooltip(index) {
+        $('#indicator a').removeClass('on');
+        $('#indicator a').eq(index).addClass('on');
+    }
+
+    $('.section').mousewheel(function (e, delta) {
+        if (delta > 0) {
+            // 마우스휠을 위로 올림
+            try {
+                tooltip($(this).index() - 1);
+                let prev = $(this).prev().offset().top;
+                console.log(prev);
+                $('html').stop().animate({ scrollTop: prev });
+            } catch (err) {
+                return false;
+            }
+        } else if (delta < 0) {
+            // 마우스휠을 아래로 내림
+            try {
+                tooltip($(this).index() + 1);
+                let next = $(this).next().offset().top;
+                console.log(next);
+                $('html').stop().animate({ scrollTop: next });
+            } catch (err) {
+                return false;
+            }
         }
     });
 });
